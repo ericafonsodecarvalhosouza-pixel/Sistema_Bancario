0b9c2ae6d4e8dc6b1a82889f9fc5ee8b6835625e
madudoll <maduaraujoumari.com>
2026-05-30 19:40:17 -0300

    tela de login e tela inicial apenas o css

---
+++ src/main/java/com/example/sistema_bancario/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.sistema_bancario.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private Hyperlink linkCadrastro;
+
+    @FXML
+    private PasswordField passwordField;
+
+
+
+
+}
