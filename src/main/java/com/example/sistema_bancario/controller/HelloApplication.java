d3979fd1d8074e2d4328de7bea878458ff5bd47c
Maria Jamile de Carvalho Albuquerque <jamilicarvalho933@gmail.com>
2026-05-30 19:57:30 -0300

    Tela de Cadastro,saque e extrato

--- src/main/java/com/example/sistema_bancario/controller/HelloApplication.java
+++ src/main/java/com/example/sistema_bancario/controller/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com.example.sistema_bancario/cadastro.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("paginacadastro!");
         stage.setScene(scene);
         stage.show();
     }
