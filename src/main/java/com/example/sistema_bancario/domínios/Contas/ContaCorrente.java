44b8763d431a2067c8c7f037120429c058b953e6
camilagimed-ui <camilagimed@gmail.com>
2026-06-01 16:55:32 -0300

    arrumando classes

--- src/main/java/com/example/sistema_bancario/domínios/Contas/ContaCorrente.java
+++ src/main/java/com/example/sistema_bancario/domínios/Contas/ContaCorrente.java
@@ -3,6 +3,7 @@ package com.example.sistema_bancario.domínios.Contas;
 import com.example.sistema_bancario.domínios.cliente.Cliente;
 import com.example.sistema_bancario.enums.TipoMovimentacao;
 import com.example.sistema_bancario.exceptions.saldoInsuficienteException;
+import com.example.sistema_bancario.exceptions.valorInvalidoException;
 
 public class ContaCorrente extends Conta {
 
@@ -24,13 +25,16 @@ public class ContaCorrente extends Conta {
     @Override
     public void sacar(double valor) {
 
+        if(valor <= 0){
+            throw new valorInvalidoException("o saque deve ser maior que zero!");
+        }
         double saldoDisponivel = getSaldo() + limiteChequeEspecial;
 
         if (valor > saldoDisponivel){
-            throw new saldoInsuficienteException("Valor de saque maior que o seu saldo disponível.");
+            throw new saldoInsuficienteException("Valor do saque maior que seu saldo disponivel");
         }
 
-        setSaldo(saldoDisponivel - valor);
+        setSaldo(getSaldo() - valor);
 
         criarAMovimentacao(TipoMovimentacao.SAQUE, valor, "Saque Realizado");
     }
