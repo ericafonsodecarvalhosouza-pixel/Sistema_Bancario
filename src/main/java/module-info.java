b9067fc524dba3968b2b02b994c0bc1acd0b81db
Maria Jamile de Carvalho Albuquerque <jamilicarvalho933@gmail.com>
2026-06-04 16:32:47 -0300

    resolvendo bugs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,6 @@ module com.example.sistema_bancario {
     opens com.example.sistema_bancario.controllers to javafx.fxml;
     exports com.example.sistema_bancario.Main;
     opens com.example.sistema_bancario.Main to javafx.fxml;
+    exports com.example.sistema_bancario.controllers;
+    opens com.example.sistema_bancario.controllers to javafx.fxml;
 }
