28fdd6fd04b41e1d65a34e51cc332191478191f0
ericafonsodecarvalhosouza-pixel <ericafonsodecarvalhosouza@gmail.com>
2026-06-03 22:06:25 -0300

    Atualizando a LoginController

--- src/main/java/com/example/sistema_bancario/controller/Controllersinterfaces/TransferenciaController.java
+++
@@ -1,4 +0,0 @@
-package com.example.sistema_bancario.controller.Controllersinterfaces;
-
-public class TransferenciaController {
-}
--- src/main/java/com/example/sistema_bancario/domínios/Banco.java
+++ src/main/java/com/example/sistema_bancario/domínios/Banco.java
@@ -1,22 +1,57 @@
 package com.example.sistema_bancario.domínios;
 
 import com.example.sistema_bancario.domínios.Contas.Conta;
+import com.example.sistema_bancario.domínios.Contas.ContaCorrente;
+import com.example.sistema_bancario.domínios.Contas.ContaPoupanca;
+import com.example.sistema_bancario.domínios.cliente.Cliente;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
+
+    public static List<Conta> cadastroContas = new ArrayList<>();
+
     public static Conta contaLogada;
 
-     public static List<Conta> cadastroContas = new ArrayList<>();
+    static {
 
-     public static Conta buscarCPF(String cpf){
-         for (Conta conta: cadastroContas){
-             if(conta.getCliente().getCPF().equals(cpf)){
-                 return conta;
-             }
-         }
-         return null;
-     }
+        Cliente cliente1 = new Cliente(
+                "João Silva",
+                "joao@gmail.com",
+                "11111111111",
+                "senha123"
+        );
 
+        ContaCorrente conta1 = new ContaCorrente(
+                cliente1,
+                1000
+        );
+
+        Cliente cliente2 = new Cliente(
+                "Maria Souza",
+                "maria@hotmail.com",
+                "22222222222",
+                "senha123"
+        );
+
+        ContaPoupanca conta2 = new ContaPoupanca(
+                cliente2
+        );
+
+        cadastroContas.add(conta1);
+        cadastroContas.add(conta2);
+    }
+
+    public static Conta buscarCPF(String cpf) {
+
+        for (Conta conta : cadastroContas) {
+
+            if (conta.getCliente().getCPF().equals(cpf)) {
+                return conta;
+            }
+        }
+
+        return null;
+    }
 }
