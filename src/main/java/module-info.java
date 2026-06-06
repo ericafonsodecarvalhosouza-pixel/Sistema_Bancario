c1b828cd91b4b83039f9b497fd739aed4f902a4f
ericafonsodecarvalhosouza-pixel <ericafonsodecarvalhosouza@gmail.com>
2026-06-05 23:45:41 -0300

    Corrigindo bug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,9 @@
 module com.example.sistema_bancario {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.example.sistema_bancario;
+
     requires java.desktop;
 
-
-    opens com.example.sistema_bancario to javafx.fxml;
-<<<<<<< HEAD
-    exports com.example.sistema_bancario;
-    exports com.example.sistema_bancario.controllers;
-    opens com.example.sistema_bancario.controllers to javafx.fxml;
-    exports com.example.sistema_bancario.Main;
-    opens com.example.sistema_bancario.Main to javafx.fxml;
-    exports com.example.sistema_bancario.controllers;
-    opens com.example.sistema_bancario.controllers to javafx.fxml;
-=======
     exports com.example.sistema_bancario.controller;
     opens com.example.sistema_bancario.controller to javafx.fxml;
     exports com.example.sistema_bancario.Main;
@@ -23,5 +12,5 @@ module com.example.sistema_bancario {
     opens com.example.sistema_bancario.controller.ServicosController to javafx.fxml;
     exports com.example.sistema_bancario.controller.LoginECadastroControllers;
     opens com.example.sistema_bancario.controller.LoginECadastroControllers to javafx.fxml;
->>>>>>> master
+
 }
